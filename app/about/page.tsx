36e37951b7e78a422ce5767de66984d2030fc6bb
Thế Lực <theluc.1746@gmail.com>
2025-08-03 23:26:33 +0700

    Test about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -2,8 +2,10 @@ import { title } from "@/components/primitives";
 
 export default function AboutPage() {
   return (
-    <div>
-      <h1 className={title()}>About</h1>
-    </div>
+    <main className="min-h-screen px-[18px] pt-20 md:pt-[18px] md:ml-[calc(min(100vw*0.25,450px)+18px)] flex flex-col gap-[18px] pb-[18px]">
+      <div>
+        <h1 className={title()}>About</h1>
+      </div>
+    </main>
   );
 }
